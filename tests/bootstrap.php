2c0f3d01b30a906d846a1a4ae1e137f0697a3db0
gotterdemarung <gotterdemarung@gmail.com>
2013-10-21 01:17:28 +0300

    path fixed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,8 +7,7 @@ spl_autoload_register(function($className){
     if (substr($className, 0, strlen(__NAMESPACE__)) !== __NAMESPACE__) {
         return;
     }
-
-    include __DIR__ . '/src/' . substr(str_replace('\\', '/', $className), strlen(__NAMESPACE__) + 1) . '.php';
+    include __DIR__ . '/unit/' . substr(str_replace('\\', '/', $className), strlen(__NAMESPACE__) + 1) . '.php';
 });
 
 // Loading widgets autoloader
