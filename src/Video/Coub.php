edba93f3b887ed948c7f3af7460f15633f5cd4c6
gotterdemarung <gotterdemarung@gmail.com>
2013-10-20 22:45:30 +0300

    init

---
+++ src/Video/Coub.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace PHPocket\Widgets\Video;
+
+
+use PHPocket\Widgets\Widget;
+
+class Coub extends Widget
+{
+    /**
+     * Returns value of widget in requested context
+     *
+     * @param int $context
+     *
+     * @return string|null Null returned for no content
+     */
+    public function getValue($context)
+    {
+        // TODO 
+    }
+
+}
