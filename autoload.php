27b845807d74804f03be85e3c374050aeb251246
gotterdemarung <gotterdemarung@gmail.com>
2013-10-21 22:55:15 +0300

    phpcs

--- autoload.php
+++ autoload.php
@@ -3,10 +3,19 @@
 namespace PHPocket\Widgets;
 
 // Autoloader for widgets
-spl_autoload_register(function($className){
-    if (substr($className, 0, strlen(__NAMESPACE__)) !== __NAMESPACE__) {
-        return;
-    }
+spl_autoload_register(
+    function($className)
+    {
+        if (substr($className, 0, strlen(__NAMESPACE__)) !== __NAMESPACE__) {
+            return;
+        }
 
-    include __DIR__ . '/src/' . substr(str_replace('\\', '/', $className), strlen(__NAMESPACE__) + 1) . '.php';
-});
+        include __DIR__
+            . '/src/'
+            . substr(
+                str_replace('\\', '/', $className),
+                strlen(__NAMESPACE__) + 1
+            )
+            . '.php';
+    }
+);
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,12 +3,21 @@
 namespace PHPocket\Widgets\Tests;
 
 // Registering own tests autoloader
-spl_autoload_register(function($className){
-    if (substr($className, 0, strlen(__NAMESPACE__)) !== __NAMESPACE__) {
-        return;
+spl_autoload_register(
+    function($className)
+    {
+        if (substr($className, 0, strlen(__NAMESPACE__)) !== __NAMESPACE__) {
+            return;
+        }
+        include __DIR__
+            . '/unit/'
+            . substr(
+                str_replace('\\', '/', $className),
+                strlen(__NAMESPACE__) + 1
+            )
+            . '.php';
     }
-    include __DIR__ . '/unit/' . substr(str_replace('\\', '/', $className), strlen(__NAMESPACE__) + 1) . '.php';
-});
+);
 
 // Loading widgets autoloader
 require __DIR__ . '/../autoload.php';
