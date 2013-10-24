80688bd1b4a7497665cbb4bc2b5d7995c24ac94d
gotterdemarung <gotterdemarung@gmail.com>
2013-10-24 21:10:14 +0300

    phpcs added

--- bin/inspect.sh
+++ bin/inspect.sh
@@ -10,6 +10,11 @@ echo "====================================="
 vendor/bin/phpmd src/ text codesize,unusedcode,naming
 
 echo
+echo "PHP Code Sniffer inspection"
+echo "==========================="
+vendor/bin/phpcs --report=summary --standard=Zend src/
+
+echo
 echo "PHPUnit testing"
 echo "==============="
 vendor/bin/phpunit -c tests --process-isolation --strict
