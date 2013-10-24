dff4f5aafe83617872a7d44b559eef4b9b1a8b54
gotterdemarung <gotterdemarung@gmail.com>
2013-10-24 20:07:36 +0300

    composer folder fix

--- autoload.php
+++ autoload.php
@@ -6,10 +6,16 @@ namespace PHPocket\Widgets;
 spl_autoload_register(
     function($className)
     {
+        // Validating namespace
         if (substr($className, 0, strlen(__NAMESPACE__)) !== __NAMESPACE__) {
             return;
         }
+        // Excluding tests
+        if (strpos($className, __NAMESPACE__ . '\\Tests') === 0) {
+            return;
+        }
 
+        // Including folder
         include __DIR__
             . '/src/'
             . substr(
